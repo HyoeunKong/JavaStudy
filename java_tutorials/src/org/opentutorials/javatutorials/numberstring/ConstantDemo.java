302d7db57bb3cd67a9336c2236cceaae67193a56
HyoeunKong <hn01360@naver.com>
2019-12-13 16:20:39 +0900

    second commit

---
+++ java_tutorials/src/org/opentutorials/javatutorials/numberstring/ConstantDemo.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		float a = 2.2F;
+		long b = 21409890823L;
+		byte c = 100;
+		short d = 200;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		int A = 3;
+		float B = 1.0F;
+		double C = A+ B;
+		System.out.println(C);
+	}
+
+}
